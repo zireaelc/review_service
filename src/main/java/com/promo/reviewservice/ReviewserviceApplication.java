8658ad22aaae166c5baf59ccd4e29db04e82153d
halina <fedorenko809@yandex.ru>
2024-10-04 14:34:30 +0400

    initial commit

---
+++ src/main/java/com/promo/reviewservice/ReviewserviceApplication.java
@@ -0,0 +1,13 @@
+package com.promo.reviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/promo/reviewservice/ReviewserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promo.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
