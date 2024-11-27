f3a01752141bea7d660e7cd123fd082685bf5775
halina <fedorenko809@yandex.ru>
2024-11-27 16:09:20 +0400

    upd migration

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,17 +1,15 @@
--- V1__create_tables.sql
+-- V001__create_tables.sql
 
 -- Таблица category
 CREATE TABLE category (
     id UUID PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
+    name VARCHAR(255) NOT NULL
 );
 
 -- Таблица subcategory
 CREATE TABLE subcategory (
     id UUID PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL,
-    category_id UUID,
-    FOREIGN KEY (category_id) REFERENCES category(id)
+    name VARCHAR(255) NOT NULL
 );
 
 -- Таблица review
@@ -19,16 +17,14 @@ CREATE TABLE review (
     id UUID PRIMARY KEY,
     text TEXT,
     rating INT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    subcategory_id UUID,
-    FOREIGN KEY (subcategory_id) REFERENCES subcategory(id)
+    created_at TIMESTAMP NOT NULL
 );
 
 -- Таблица users
 CREATE TABLE users (
     id UUID PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL
 );
